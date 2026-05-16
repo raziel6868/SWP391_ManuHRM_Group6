fa3b6b47c50940a4ff88690ae958890f79643e50
raziel6868 <minhluonganh@gmail.com>
2026-05-16 17:41:39 +0700

    feat: configure utilities and environment loader

--- src/main/java/util/AppConfig.java
+++ src/main/java/util/AppConfig.java
@@ -1,4 +1,28 @@
 package util;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 public class AppConfig {
+    private static Dotenv dotenv;
+
+    static {
+        try {
+            dotenv = Dotenv.configure()
+                    .ignoreIfMissing()
+                    .load();
+        } catch (Exception e) {
+            System.err.println("==========================================================================");
+            System.err.println("WARNING: COULD NOT LOAD ENVIRONMENT VARIABLES!");
+            System.err.println("Please check your '.env' file.");
+            System.err.println("==========================================================================");
+        }
+    }
+
+    public static String get(String key) {
+        return dotenv != null ? dotenv.get(key) : null;
+    }
+
+    public static String get(String key, String defaultValue) {
+        return dotenv != null ? dotenv.get(key, defaultValue) : defaultValue;
+    }
 }

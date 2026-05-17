49a5ed851ae3fe09a6db7b694ce9b619d93c3a25
Lương Anh Minh K18 <minhluonganh@gmail.com>
2026-05-17 17:50:52 +0700

    chore: Test workflow 2 (#3)
    * chore: Test workflow 2
    
    * Set fetch-depth to 0 in CI workflow

--- src/main/java/model/Permission.java
+++ src/main/java/model/Permission.java
@@ -6,4 +6,44 @@ public class Permission {
     private String name;
     private String urlPattern;
     private String module;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrlPattern() {
+        return urlPattern;
+    }
+
+    public void setUrlPattern(String urlPattern) {
+        this.urlPattern = urlPattern;
+    }
+
+    public String getModule() {
+        return module;
+    }
+
+    public void setModule(String module) {
+        this.module = module;
+    }
 }
